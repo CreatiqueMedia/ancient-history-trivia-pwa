12902fdbb3992e909db4bbf156fb3678745afd37
Ron Ratzlaff <ron@creatiquemedia.com>
2025-06-21 16:06:02 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ancient-history-trivia-pwa/sw.js', { scope: '/ancient-history-trivia-pwa/' })})}
